3c010706fad748d424229432387a498abcd62876
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-01 02:24:33 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,11 +4,11 @@ class App < Sinatra::Base
 
   get '/user_input' do
     @user_input = params[:user_phrase]
-    erb user_input
+    erb :user_input
   end
 
   post '/user_input' do
-
+    
   end
 
 end
